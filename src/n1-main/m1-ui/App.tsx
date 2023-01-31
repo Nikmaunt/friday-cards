5b660bd65939f0dbe2a4bf662ec70e46db6a025f
Denishilo <denisanishenko@gmail.com>
2023-01-31 23:20:26 +0300

    add common components

--- src/n1-main/m1-ui/App.tsx
+++ src/n1-main/m1-ui/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./header/header";
+import {Route, Routes} from "react-router-dom";
 
 const App = () => {
     return (
         <div className="App">
-            //HashRouter, Provider
             <>
-                {/*<Header/>*/} // navlink
+                <Header/>
+                <div>
+                    <Routes>
+                        <Route path={'/login'} element={<div>LOGIN</div>}></Route>
+                        <Route path={'/'} element={<div>LOGIN</div>}></Route>
+                    </Routes>
+                </div>
             </>
         </div>
     );
