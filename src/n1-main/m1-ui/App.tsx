0e8a99eb0e3b9e695eaa7c69e1342f173074386b
Denishilo <denisanishenko@gmail.com>
2023-01-31 21:29:09 +0300

    innit commit

---
+++ src/n1-main/m1-ui/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            //HashRouter, Provider
+            <>
+                {/*<Header/>*/} // navlink
+            </>
+        </div>
+    );
+}
+
+export default App;
