4df0203a12d2bb7359b015c77b895bd027f89c70
Nick <nikmaunt@gmail.com>
2023-02-08 19:08:55 +0300

    refactor(Header): Routes removed, new styles

--- src/common/header/header.tsx
+++ src/common/header/header.tsx
@@ -1,84 +1,46 @@
+import * as React from 'react';
 import s from "./Header.module.css";
-import { Navigate, NavLink } from "react-router-dom";
-import { SuperButton } from "../superButton/superButton";
-import { useAppDispatch, useAppSelector } from "../../app/store";
-import { setLoginUser } from "../loginRegistration/authReducer";
-import {Stack} from "@mui/material";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
-import React, {useEffect} from "react";
+import {Stack, Tooltip} from "@mui/material";
+import {Navigate} from "react-router-dom";
 import userPhoto from '../../feature/profile/img/userPhoto.png';
-import {useAppDispatch, useAppSelector} from "../../app/store";
-import {UserDataType} from "../loginRegistration/authReducer";
-import {updateUser} from "../../feature/profile/profileReducer";
+import {useAppSelector} from "../../app/store";
+import {SuperButton} from "../superButton/superButton";
 
-export const Header = () => {
+export const  Header = () => {
+  const userName = useAppSelector<string>((state) => state.profile.name)
   const isLogin = useAppSelector<boolean>((state) => state.auth.isLogin);
-  const dispatch = useAppDispatch();
-
-  let activeStyle = {
-    color: "#0059b2",
-  };
   const goToSignIn = () => {
-    console.log("bottom");
     return <Navigate to={"/friday-cards/login"} />;
   };
-  const logoutHandler = () => {
-    dispatch(setLoginUser(false));
-  };
-
-  const userData = useAppSelector<UserDataType | undefined>((state) => state.auth.userData)
-  const userName = useAppSelector<string>((state) => state.profile.userName)
-  const isSignUp = useAppSelector<boolean>((state) => state.auth.isAuth);
-
-  // не понятно как типизировать, оставим так, потом все равно этот header убирать
-  // @ts-ignore
-  const style = ({ isActive }) => (isActive ? activeStyle : undefined);
-   console.log(userData?.name)
-
   return (
-    <div className={s.wrapper}>
-      <div className={s.list}>
-        <NavLink style={style} to="/friday-cards/login">
-          Login
-        </NavLink>
-        <NavLink style={style} to="/friday-cards/registration">
-          Registration
-        </NavLink>
-        <NavLink style={style} to="/friday-cards/profile">
-          Profile
-        </NavLink>
-        <NavLink style={style} to="/friday-cards/404">
-          Page Not Found
-        </NavLink>
-        <NavLink style={style} to="/friday-cards/password-recovery">
-          Password Recovery
-        </NavLink>
-        <NavLink style={style} to="/friday-cards/new-password">
-          New Password
-        </NavLink>
-        <NavLink style={style} to="/friday-cards/test-components">
-          Test Components
-        </NavLink>
-        <div className={s.button}>
-          {isLogin ? (
-            <SuperButton name={"Logout"} callback={logoutHandler} />
-          ) : (
-            <SuperButton name={"Sign in"} callback={goToSignIn} />
-          )}
-        </div>
-        {isSignUp ?  <Stack direction="row" spacing={1}>
-          <h4  >{userName}</h4>
-          <Avatar style={{marginTop:'12px'}}
-                  alt="userName"
-                  src={userPhoto}
-                  sx={{ width: 36, height: 36}}
-          />
-        </Stack> :    <div className={s.button}>
-          <SuperButton name={"Sign in"} />
-        </div> }
 
-
-      </div>
-    </div>
+      <AppBar style={{backgroundColor:"white"}} position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <img className={s.main_logo} src="https://static.tildacdn.com/tild3064-6361-4562-a539-303563643237/logo-big-blue.png" alt=""/>
+            <Box sx={{ flexGrow: 1, display:'flex', justifyContent:'flex-end',  }}>
+              <Tooltip title="Open settings">
+                <div >
+                  {isLogin ?  <Stack className={s.userProfile}  direction="row" spacing={1}>
+                    <h4 style={{color:'black'}}  >{userName}</h4>
+                    <Avatar style={{marginTop:'12px'}}
+                            alt="userName"
+                            src={userPhoto}
+                            sx={{ width: 36, height: 36}}
+                    />
+                  </Stack> :<div className={s.button} >
+                    <SuperButton  name={"Sign in"} callback={goToSignIn} />
+                  </div> }
+                </div>
+              </Tooltip>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
   );
-};
+}
