1ff468fabe1fd01e684635cfbf799241395f5f05
Nick <nikmaunt@gmail.com>
2023-02-10 14:00:01 +0300

    fix: PersistGate removed

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,14 @@ import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import {persistor, store} from "./app/store";
-import {PersistGate} from "redux-persist/integration/react";
+import {store} from "./app/store";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
       <App />
-        </PersistGate>
     </Provider>
   </BrowserRouter>
 );
