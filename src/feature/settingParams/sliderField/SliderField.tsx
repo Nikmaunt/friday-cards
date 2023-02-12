e43a6f9583d0babf753d61b0143a2d27f116033e
Denishilo <denisanishenko@gmail.com>
2023-02-12 11:50:13 +0300

    feat: add slider with localStorage

--- src/feature/settingParams/sliderField/SliderField.tsx
+++ src/feature/settingParams/sliderField/SliderField.tsx
@@ -4,19 +4,14 @@ import Slider from "@mui/material/Slider";
 import s from "../searchField/SearchField.module.css";
 import styles from "./SliderField.module.css";
 import { useState } from "react";
-import { restoreState } from "../../../common/functions/localStorage/localStorage";
+import { restoreState, saveState } from "../../../common/functions/localStorage/localStorage";
 
 export const SliderField = () => {
-  const [value1, setValue1] = useState(restoreState<number>("value1", 0));
-  const [value2, setValue2] = useState(restoreState<number>("value2", 100));
+  const [value, setValue] = useState(restoreState<number[]>("slider", [0, 100]));
 
   const handlerChange = (event: Event, newValue: number | number[]) => {
-    if (Array.isArray(newValue)) {
-      setValue1(+newValue[0]);
-      setValue2(+newValue[1]);
-    } else {
-      setValue1(+newValue);
-    }
+    setValue(newValue as number[]);
+    saveState<number[]>("slider", [value[0], value[1]]);
   };
 
   return (
@@ -24,13 +19,13 @@ export const SliderField = () => {
       <h2 className={s.title}>Number of cards</h2>
       <div className={styles.sliderWrapper}>
         <div className={styles.numberWrapper}>
-          <div className={styles.spanNumber}>{value1}</div>
+          <div className={styles.spanNumber}>{value[0]}</div>
         </div>
         <Box sx={{ width: 155 }}>
-          <Slider value={[value1, value2]} onChange={handlerChange} valueLabelDisplay="auto" />
+          <Slider value={value} onChange={handlerChange} valueLabelDisplay="auto" />
         </Box>
         <div className={styles.numberWrapper}>
-          <span className={styles.spanNumber}>{value2}</span>
+          <span className={styles.spanNumber}>{value[1]}</span>
         </div>
       </div>
     </div>
