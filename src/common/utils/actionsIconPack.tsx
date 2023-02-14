7fd4b327106d1f3dbddbdb10b13c823fa2c094fa
”vladimir817vk@gmail.com” <vladimir817vk@gmail.com>
2023-02-15 01:34:10 +0300

    feat: add actionsIconPack callbacks and hover style

--- src/common/utils/actionsIconPack.tsx
+++ src/common/utils/actionsIconPack.tsx
@@ -3,19 +3,34 @@ import { userId } from "../../feature/packs/selectors";
 import SchoolOutlinedIcon from "@mui/icons-material/SchoolOutlined";
 import BorderColorOutlinedIcon from "@mui/icons-material/BorderColorOutlined";
 import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import s from "./actionsIconPack.module.css";
 import React from "react";
 
 type ActionsIconPackType = {
   user_id: string;
 };
 
+const learnPackCallback = () => {
+  alert("Learn pack");
+};
+
+const editPackCallback = () => {
+  alert("Edit pack");
+};
+
+const deletePackCallback = () => {
+  alert("Delete pack");
+};
+
 export const ActionsIconPack = (props: ActionsIconPackType) => {
   const userAuthId = useAppSelector(userId);
   return (
-    <div>
-      <SchoolOutlinedIcon />
-      {props.user_id === userAuthId ? <BorderColorOutlinedIcon /> : null}
-      {props.user_id === userAuthId ? <DeleteOutlinedIcon /> : null}
+    <div className={s.active}>
+      <span onClick={learnPackCallback}>
+        <SchoolOutlinedIcon />
+      </span>
+      <span onClick={editPackCallback}>{props.user_id === userAuthId ? <BorderColorOutlinedIcon /> : null}</span>
+      <span onClick={deletePackCallback}>{props.user_id === userAuthId ? <DeleteOutlinedIcon /> : null}</span>
     </div>
   );
 };
