b97232c490b3dec4249928c90f2b5196a3033ae8
Nick <nikmaunt@gmail.com>
2023-02-16 01:49:38 +0300

    feat: ClearIcon added

--- src/feature/settingParams/clearButton/clearButton.tsx
+++ src/feature/settingParams/clearButton/clearButton.tsx
@@ -1,11 +1,13 @@
 import clearButton from "../../../img/CearButton.svg";
+import ClearIcon from '@mui/icons-material/Clear';
 import React from "react";
 import s from "../SettingParams.module.css";
 export const ClearButton = () => {
   return (
     <div className={s.wrapperClearButton}>
       <div>
-        <img className={s.imgClearButton} src={clearButton} alt="clear" />
+        {/*<img className={s.imgClearButton} src={clearButton} alt="clear" />*/}
+          <ClearIcon/>
       </div>
     </div>
   );
