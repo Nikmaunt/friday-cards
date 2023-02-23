9eb0a8e80d2b5ba029ecc19666fff6b6b2a83fbf
Nick <nikmaunt@gmail.com>
2023-02-23 21:08:51 +0300

    feat: new icons added

--- src/common/header/header.tsx
+++ src/common/header/header.tsx
@@ -40,22 +40,3 @@ export const Header = () => {
   );
 };
 
-// return (
-//     <div className={s.topNav}>
-//         <img
-//             className={s.mainLogo}
-//             src="https://static.tildacdn.com/tild3064-6361-4562-a539-303563643237/logo-big-blue.png"
-//             alt="header_logo"
-//         />
-//         <div>
-//             {isLogin ? (
-//                 <HeaderDropdown/>
-//             ) : (
-//                 <div className={s.button}>
-//                     <SuperButton name={"Sign in"} callback={goToSignIn} />
-//                 </div>
-//             )}
-//         </div>
-//     </div>
-// );
-
