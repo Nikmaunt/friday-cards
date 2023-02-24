44ea6bb9315b74cc5d0439cd82d4fef021e4ab51
Nick <nikmaunt@gmail.com>
2023-02-25 02:55:54 +0300

    feat:titleDropdown added

--- src/common/titleWithButton/titleWithButton.tsx
+++ src/common/titleWithButton/titleWithButton.tsx
@@ -3,39 +3,37 @@ import {SuperButton} from "../superButton/superButton";
 import React from "react";
 import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
 import PATH from "../constans/path/path";
-import {getAllUserCards} from "../../feature/cards/cardsReducer";
 import {useNavigate, useParams} from "react-router-dom";
 import {useAppDispatch} from "../../app/store";
 import {useSelector} from "react-redux";
 import {selectorUserId} from "../../app/appSelectors";
-import {selectorPackUserId} from "../../feature/cards/cardsSelectors";
+import {selectorPackName, selectorPackUserId} from "../../feature/cards/cardsSelectors";
+import {TitleDropdown} from "./titleDropdown";
 
 export const TitleWithButton = (props: TitleWithButtonPropsType) => {
-  const { title, nameButton, callback } = props;
-  const { id } = useParams();
-  const userAuthId = useSelector(selectorUserId);
-  const userPackId = useSelector(selectorPackUserId);
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
-  const learnPackHandler = () => {
-    navigate(`${PATH.LEARN_PACK}${id}`);
-    if (id) {
-      dispatch(getAllUserCards(id));
-    }
-  };
-  return (
-    <div className={s.titleButton}>
-      <h2>{title}</h2>
-      {id && (
-        <span className={s.learnIcon} onClick={learnPackHandler}>
-          <SchoolOutlinedIcon className={s.learnIcon} />
-        </span>
-      )}
-      <div className={s.button}>
-        <SuperButton name={nameButton} callback={callback} />
-      </div>
-    </div>
-  );
+    const {title, nameButton, callback} = props;
+    const {id} = useParams();
+    const packName = useSelector(selectorPackName);
+    const userAuthId = useSelector(selectorUserId);
+    const userPackId = useSelector(selectorPackUserId);
+    const navigate = useNavigate();
+    const learnPackHandler = () => {
+        navigate(`${PATH.LEARN_PACK}${id}`);
+    };
+    console.log(userAuthId === userPackId)
+    console.log(id === userPackId )
+    return (
+        <div className={s.titleButton}>
+            <h2>{id === userPackId ? packName : title}</h2>
+            {id  && < TitleDropdown pack_id={id} pack_name={packName}/>  }
+            {id && (
+                <button className={s.learnIcon} onClick={learnPackHandler}><SchoolOutlinedIcon /> </button>
+            )}
+            <div className={s.button}>
+                <SuperButton name={nameButton} callback={callback}/>
+            </div>
+        </div>
+    );
 };
 export type TitleWithButtonPropsType = {
     title: string;
