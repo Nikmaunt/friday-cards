aa49a00cf2ba903c9323b91dfbb91b127262a75d
”vladimir817vk@gmail.com” <vladimir817vk@gmail.com>
2023-03-03 02:19:54 +0300

    feat: add sort

--- src/feature/packs/packsTableHead.tsx
+++ src/feature/packs/packsTableHead.tsx
@@ -11,19 +11,71 @@ import { DataRows } from "./packsTable";
 import { useAppDispatch } from "../../app/store";
 import { visuallyHidden } from "@mui/utils";
 import Box from "@mui/material/Box";
+import { fetchPacksTC } from "./packsReducer";
+import { log } from "util";
+
 export const PacksTableHead = () => {
   const [order, setOrder] = React.useState<Order>("asc");
-  const [orderBy, setOrderBy] = React.useState<keyof DataRows>("cards");
+  const [orderBy, setOrderBy] = React.useState<string>("cards");
+  const dispatch = useAppDispatch();
 
-  const onRequestSort = (event: React.MouseEvent<unknown>, property: keyof DataRows) => {
+  const onRequestSort = (event: React.MouseEvent<unknown>, property: string) => {
     const isAsc = orderBy === property && order === "asc";
+    //0 от большего desk
+    //1 от меньшего к большему ask
+    if (orderBy === property) {
+      let params;
+      if (order === "asc") {
+        console.log(order);
+        // sortingDirection = 1;
+        params = { sortPacks: `1${property}` };
+        setOrder("desc");
+        console.log("setOrder", order);
+      } else {
+        // isAsc = false;
+        // sortingDirection = 0;
+        // params = { sortPacks: `0${property}` };
+        params = { sortPacks: `0${property}` };
+        console.log("setOrder", order);
+        setOrder("asc");
+      }
+      dispatch(fetchPacksTC(params));
+    }
     setOrder(isAsc ? "desc" : "asc");
+    console.log("orderBy", orderBy);
+    console.log("property", property);
+    console.log("order", order);
+    console.log("isAsc logic", order, "=", property, "&&", order);
+    // console.log("isAsc", isAsc);
+    //ask по возрастанию
     setOrderBy(property);
+    // const params = { sortPacks: property };
+    // dispatch(fetchPacksTC(params));
   };
   const createSortHandler = (property: keyof DataRows) => (event: React.MouseEvent<unknown>) => {
-    onRequestSort(event, property);
-    const params = { sortPacks: "1updated" };
-    //dispatch(setPacksParams(params));
+    let propertyKey: string;
+    if (property === "cards") {
+      propertyKey = "cardsCount";
+    }
+    if (property === "lastUpdated") {
+      propertyKey = "updated";
+    }
+    if (property === "name") {
+      propertyKey = "name";
+    }
+
+    // switch (property) {
+    //   case "cards":
+    //     return (propertyKey = "cardsCount");
+    //   case "lastUpdated":
+    //     return (propertyKey = "updated");
+    //   default:
+    //     return propertyKey = property;
+    // }
+    // console.log("property", property);
+    // @ts-ignore
+    onRequestSort(event, propertyKey);
+    // dispatch(setPacksParams(params));
   };
   return (
     <TableHead className={s.headCell}>
@@ -37,11 +89,12 @@ export const PacksTableHead = () => {
               onClick={createSortHandler(headCell.id)}
             >
               {headCell.label}
-              {orderBy === headCell.id ? (
-                <Box component="span" sx={visuallyHidden}>
-                  {order === "desc" ? "sorted descending" : "sorted ascending"}
-                </Box>
-              ) : null}
+              {/*"2"*/}
+              {/*{orderBy === headCell.id ? (*/}
+              {/*  <Box component="span" sx={visuallyHidden}>*/}
+              {/*    {order === "desc" ? "sorted descending" : "sorted ascending"}*/}
+              {/*  </Box>*/}
+              {/*) : null}*/}
             </TableSortLabel>
           </TableCell>
         ))}
